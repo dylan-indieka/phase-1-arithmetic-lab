b2022687d5bc3fbab8f4a6875d72ac03c633a36f
dylan-indieka <dylan.indieka@student.moringaschool.com>
2025-03-03 04:18:33 -0800

    adding files

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Define num1 and num2 for the multiply function
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// Define the random function to generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Define num3 and num4 for the mod function
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+// Define the max function to return the highest number in a set
+const max = Math.max(1, 2, 3, 4, 20);
